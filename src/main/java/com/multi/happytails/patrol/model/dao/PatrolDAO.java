597a128dd98519d5d8324afeb70d8dc21e727e99
wjh0429 <72801896+wjh0429@users.noreply.github.com>
2024-07-23 17:12:12 +0900

    test

--- src/main/java/com/multi/happytails/patrol/model/dao/PatrolDAO.java
+++ src/main/java/com/multi/happytails/patrol/model/dao/PatrolDAO.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Mapper
 public interface PatrolDAO {
 
-    public int patrolInsert(PatrolDTO patrolDTO);
+    int patrolInsert(PatrolDTO patrolDTO);
 
     List<PatrolDTO> findAllPatrol();
 
