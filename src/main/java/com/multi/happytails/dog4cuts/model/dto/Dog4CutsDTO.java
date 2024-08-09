37a02179f8cee371f262c02e3a48e744ff0dd6ab
wjh0429 <72801896+wjh0429@users.noreply.github.com>
2024-08-09 17:21:45 +0900

    UI수정

--- src/main/java/com/multi/happytails/dog4cuts/model/dto/Dog4CutsDTO.java
+++ src/main/java/com/multi/happytails/dog4cuts/model/dto/Dog4CutsDTO.java
@@ -1,6 +1,8 @@
 package com.multi.happytails.dog4cuts.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Timestamp;
 
@@ -20,5 +22,7 @@ public class Dog4CutsDTO {
     private int dog4cutsNo;
     private int userNo;
     private String userId;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private Timestamp createtime;
 }
