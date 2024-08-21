6fd9e59707b6b9fceb814fed8a35dc7d9c50c624
wjh0429 <72801896+wjh0429@users.noreply.github.com>
2024-08-21 11:44:43 +0900

    UI 수정, 회원관리 검색추가

--- src/main/java/com/multi/happytails/patrol/service/PatrolService.java
+++ src/main/java/com/multi/happytails/patrol/service/PatrolService.java
@@ -34,8 +34,8 @@ public class PatrolService {
         return patrolDAO.findAllPatrol();
     }
 
-    public PatrolDTO findOnePatrol(int userNo, String name) {
-        return patrolDAO.findOnePatrol(userNo, name);
+    public PatrolDTO findOnePatrol(int userNo) {
+        return patrolDAO.findOnePatrol(userNo);
     }
 
     public PatrolDTO findOnePatrolByPatrolNo(int patrolNo) {
