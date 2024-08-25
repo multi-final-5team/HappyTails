feb041173111f45b0df7ce37ca09c3da71de74e4
ShinHyeoncheol <guscjf5806@icloud.com>
2024-08-26 00:39:42 +0900

    Fix: UI

--- src/main/java/com/multi/happytails/config/ErrorControllerConfig.java
+++ src/main/java/com/multi/happytails/config/ErrorControllerConfig.java
@@ -1,6 +1,7 @@
 package com.multi.happytails.config;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  * packageName    : com.multi.happytails.config
@@ -16,9 +17,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class ErrorControllerConfig {
 
-//    @ExceptionHandler
-//    public String handleBadRequestException(Exception e) {
-//
-//        return "/error/error";
-//    }
+    @ExceptionHandler
+    public String handleBadRequestException(Exception e) {
+
+        return "/error/error";
+    }
 }
